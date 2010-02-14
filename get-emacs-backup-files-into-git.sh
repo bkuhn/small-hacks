a1e7bba51f0d4f0eb7eb6eee2196b1f60af3088f
Bradley M. Kuhn <bkuhn@ebb.org>
2010-02-14 13:21:51 -0500

    This file as it stood at my last edit of it back in 2010.

--- get-emacs-backup-files-into-git.sh
+++ get-emacs-backup-files-into-git.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 for i in `ls -r1t $1.~*~`; do
-    date=`stat -c '%y' $i`
+    date=`stat -c '%y' $i | perl -pe 's/(\.\d+) / /'`
     logdate=`stat -c '%y' $i|perl -pe 's/^(\S+)\s+.*$/$1/;'`
-    echo $date $i
+    echo $logdate $date $i
     cp -pa $i $2
     export GIT_AUTHOR_DATE="$date"
     git commit -m "Brought in file from ~ backup files, as it was on $logdate" $2
